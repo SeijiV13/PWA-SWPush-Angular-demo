36579acc014292a9b9443217f04b822a45d76921
Seiji Ralph Villafranca <seijiv13@outlook.com>
2020-08-12 22:19:19 +0800

    fix footer

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { SwPush } from '@angular/service-worker';
+import { WebNotificationService } from './../services/web-notification.service';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
+export class FooterComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  isEnabled = this.swPush.isEnabled;
+  isGranted = Notification.permission === 'granted';
+constructor(private swPush: SwPush,
+            private webNotificationService: WebNotificationService) {}
+submitNotification(): void {
+    this.webNotificationService.subscribeToNotification();
   }
-
 }
